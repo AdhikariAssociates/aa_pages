466a5243f21496718026a2964a6d56b0fb0b83d0
AdhikariAssociates <adhikariassociates2016@gmail.com>
2016-08-23 07:28:27 +0530

    Create index.php

---
+++ index.php
@@ -0,0 +1,11 @@
+<!doctype html>
+<html>
+	<head>
+		<title>Title...</title>
+	</head>
+	<body>
+		
+<p style="text-align:center;"><span style="font-family:Cursive;font-size:22px;font-style:normal;font-weight:normal;text-decoration:blink;text-transform:capitalize;color:000000;">Adhikari Associates</span>
+</p>
+	</body>
+</html>
